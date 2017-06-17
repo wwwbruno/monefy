3c9cec5bddc166d9d76d851466a9b06c48fb7161
Bruno Almeida <bruno@disvolvi.com>
2017-06-17 16:04:25 -0300

    Document Matchers module

--- lib/monefy/matchers.rb
+++ lib/monefy/matchers.rb
@@ -1,17 +1,59 @@
 class Monefy
+  # Encapsulate all the logic to compare differente Monefy instances
   module Matchers
+
+    # Check if two distinct Monefy instances are equal.
+    #
+    # @param money [Monefy] another Monefy instance.
+    #
+    # @return [Boolean] true if both instances are equal and false if they
+    # are different.
+    #
+    # @example
+    #   Monefy.new(50, 'EUR') == Monefy.new(40, 'EUR') # => false
+    #   Monefy.new(50, 'EUR') == Monefy.new(55.5, 'USD') # => true
     def == money
       amount == converted_money_currency(money)
     end
 
+    # Check if two distinct Monefy instances are different.
+    #
+    # @param money [Monefy] another Monefy instance.
+    #
+    # @return [Boolean] true if both instances are different and false if they
+    # are equal.
+    #
+    # @example
+    #   Monefy.new(50, 'EUR') != Monefy.new(40, 'EUR') # => true
+    #   Monefy.new(50, 'EUR') != Monefy.new(55.5, 'USD') # => false
     def != money
       amount != converted_money_currency(money)
     end
 
+    # Check if one Monefy instance is greater than another.
+    #
+    # @param money [Monefy] another Monefy instance.
+    #
+    # @return [Boolean] true if current instace is greater than another and
+    # false if not.
+    #
+    # @example
+    #   Monefy.new(50, 'EUR') > Monefy.new(40, 'EUR') # => true
+    #   Monefy.new(50, 'EUR') > Monefy.new(200, 'USD') # => false
     def > money
       amount > converted_money_currency(money)
     end
 
+    # Check if one Monefy instance is less than another.
+    #
+    # @param money [Monefy] another Monefy instance.
+    #
+    # @return [Boolean] true if current instace is less than another and
+    # false if not.
+    #
+    # @example
+    #   Monefy.new(50, 'EUR') < Monefy.new(40, 'EUR') # => false
+    #   Monefy.new(50, 'EUR') < Monefy.new(200, 'USD') # => true
     def < money
       amount < converted_money_currency(money)
     end
